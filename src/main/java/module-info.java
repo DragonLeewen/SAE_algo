af54a55d0334bef85e8d641127cd760fdc3e585f
DragonLeewen <leewen56300gmail.com>
2022-05-23 15:58:26 +0200

    dossier sae

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.sae_algo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.example.sae_algo to javafx.fxml;
+    exports com.example.sae_algo;
+    exports modele;
+    exports vue;
+    exports controleur;
+    opens vue to javafx.fxml;
+    opens controleur to javafx.fxml;
+}
