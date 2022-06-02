992beb4642c7f8d5de30a18665e27aebb905eab9
Mansour78 <mansoursy82@gmail.com>
2022-06-02 12:54:24 +0200

    Ajout méthode de lecture d'un scénario à partir d'un fichier.

--- src/main/java/outils/LectureEcriture.java
+++ src/main/java/outils/LectureEcriture.java
@@ -1,5 +1,29 @@
 package outils;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class LectureEcriture {
+    public static Scenario lectureScenario (File fichier) throws IOException{
+        Scenario scenario = new Scenario();
+
+        BufferedReader bufferEntree = new BufferedReader(new FileReader(fichier));
+        String ligne;
+
+        StringTokenizer tokenizer;
+        do {
+            ligne = bufferEntree.readLine();
+            if (ligne != null) {
+                tokenizer = new StringTokenizer(ligne, " ->");
+                scenario.ajoutVendeurAcheteur(tokenizer.nextToken(), tokenizer.nextToken());
+            }
+        }
+        while (ligne != null);
+        bufferEntree.close();
+        return scenario;
+    }
 
 }
