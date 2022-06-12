cdad0a780fa5ef3202a3da50addf60f71c41babe
Mansour78 <mansoursy82@gmail.com>
2022-06-12 14:08:33 +0200

    Méthode lecture ville permettant de récupérer les noms des villes et leur résident

--- src/main/java/outils/LectureEcriture.java
+++ src/main/java/outils/LectureEcriture.java
@@ -23,6 +23,24 @@ public class LectureEcriture {
         bufferEntree.close();
         return scenario;
     }
+    public static Scenario lectureVille (File fichier) throws IOException{
+        Scenario scenario = new Scenario();
+
+        BufferedReader bufferEntree = new BufferedReader(new FileReader(fichier));
+        String ligne;
+
+        StringTokenizer tokenizer;
+        do {
+            ligne = bufferEntree.readLine();
+            if (ligne != null) {
+                tokenizer = new StringTokenizer(ligne, " ");
+                scenario.ajoutVendeurAcheteur(tokenizer.nextToken(), tokenizer.nextToken());
+            }
+        }
+        while (ligne != null);
+        bufferEntree.close();
+        return scenario;
+    }
 
     public static void ecritureScenario (String nomFichier, Scenario scenario) throws IOException{
 
