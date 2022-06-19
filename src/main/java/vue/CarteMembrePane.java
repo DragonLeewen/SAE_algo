158a619ef3b300214839885cfc7f722144310e1f
DragonLeewen <leewen56300gmail.com>
2022-06-19 19:01:50 +0200

    Finalisation de CarteMembrePane

--- src/main/java/vue/CarteMembrePane.java
+++ src/main/java/vue/CarteMembrePane.java
@@ -4,33 +4,34 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import outils.LectureEcriture;
 import outils.MembresApli;
-import outils.Scenario;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeMap;
-
-import static outils.LectureEcriture.lectureVille;
 
 public class CarteMembrePane extends GridPane {
 
     private ComboBox membreSelection = new ComboBox<String>();
-    //private Image badgeFeu = new Image("badge_feu.png");
-    //private Image badgeEau = new Image("badge_eau.png");
-    //private Image badgeMetal = new Image("badge_metal.png");
-    private MembresApli membresApli = new MembresApli();
-    private TreeMap<String,String> mapMemVil = new TreeMap<>();
-    private Label ville = new Label();
-    private List<String> listVille = new ArrayList<>();
+    File filefeu = new File("C:/SAE_algo/badge_feu.png");
+    File fileau = new File("C:/SAE_algo/badge_eau.png");
+    File filemetal = new File("C:/SAE_algo/badge_metal.png");
+
+    String localUrl = filefeu.toURI().toURL().toString();
+    String localUrl2 = fileau.toURI().toURL().toString();
+    String localUrl3 = filemetal.toURI().toURL().toString();
+
+    Image badgefeu = new Image(localUrl);
+    Image badgeeau = new Image(localUrl2);
+    Image badgemetal = new Image(localUrl3);
+    private MembresApli membresApli;
+    private Label ville;
+    private List<String> listVille;
+    private Label num;
 
     public CarteMembrePane() throws IOException {
         File membreAp= new File("scenarios" + File.separator + "membres_APLI.txt");
@@ -41,30 +42,34 @@ public class CarteMembrePane extends GridPane {
             membreSelection.getItems().add(member);
         }
 
-        //Image badge = badgeFeu;
-        //if (compt%3==1){
-        //    badge = badgeEau;
-        //}
-        //if (compt%3==2){
-        //    badge = badgeMetal;
-        //}
-
         membreSelection.getSelectionModel().select(0);
         this.add(membreSelection,0,0,4,1);
         ville = new Label("Ville du membre : "+listVille.get(0));
         this.add(ville,0,1,4,1);
 
+        num = new Label("numéro de membre : "+(membreSelection.getSelectionModel().getSelectedIndex()+1));
+        this.add(num,0,2,4,1);
+
+        ImageView imageView = new ImageView(badgeeau);
+        this.add(imageView,0,3,1,1);
+
         membreSelection.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                System.out.println("Set reussi");
                 ville.setText("Ville du membre : "+listVille.get(membreSelection.getSelectionModel().getSelectedIndex()));
+                num.setText("numéro de membre : "+(membreSelection.getSelectionModel().getSelectedIndex()+1));
+
+                if (membreSelection.getSelectionModel().getSelectedIndex()%3==0){
+                    imageView.setImage(badgeeau);
+                }
+                if (membreSelection.getSelectionModel().getSelectedIndex()%3==1){
+                    imageView.setImage(badgefeu);
+                }
+                if (membreSelection.getSelectionModel().getSelectedIndex()%3==2){
+                    imageView.setImage(badgemetal);
+                }
             }
         });
-
-        HBox vboxTemps = new HBox();
-        //vboxTemps.getChildren(badgeFeu);
-        this.add(vboxTemps,2,2,3,3);
     }
 
 }
