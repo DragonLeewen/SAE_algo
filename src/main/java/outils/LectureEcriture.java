b3e30366ab9f3d94b1ba9ad055fdb886f4e68de0
Mansour78 <mansoursy82@gmail.com>
2022-06-20 02:33:00 +0200

    Commentaire des méthodes LectureEcriture, CarteMembrePane et VMainBox

--- src/main/java/outils/LectureEcriture.java
+++ src/main/java/outils/LectureEcriture.java
@@ -1,42 +1,49 @@
 package outils;
-
 import java.io.*;
 import java.util.StringTokenizer;
+
 
 public class LectureEcriture {
 
     public static Scenario lectureScenario (File fichier) throws IOException{
-        Scenario scenario = new Scenario();
 
-        BufferedReader bufferEntree = new BufferedReader(new FileReader(fichier));
+        Scenario scenario = new Scenario(); // Déclaration et Instanciation d'un objet Scenario
+        BufferedReader bufferEntree = new BufferedReader(new FileReader(fichier)); // Création du tampon qui va lire le fichier
+
         String ligne;
+        StringTokenizer tokenizer; // Déclaration du tokenizer qui est ici un groupe de lettre
 
-        StringTokenizer tokenizer;
-        do {
+        do { // Parcours d'une ligne du fichier
             ligne = bufferEntree.readLine();
-            if (ligne != null) {
-                tokenizer = new StringTokenizer(ligne, " ->");
-                scenario.ajoutVendeurAcheteur(tokenizer.nextToken(), tokenizer.nextToken());
+            if (ligne != null) { // Vérifie si une ligne est vide
+                tokenizer = new StringTokenizer(ligne, " ->"); /** Définie ce qu'est un tokenizer, ici un groupe de lettre séparé
+                 par un espace une flèche **/
+                scenario.ajoutVendeurAcheteur(tokenizer.nextToken(), tokenizer.nextToken()); /** Ajout des deux tokens Vendeurs et Acheteur
+                 à l'objet de la classe Scenario **/
             }
         }
-        while (ligne != null);
+
+        while (ligne != null); // Parcours toutes les lignes du fichier
         bufferEntree.close();
         return scenario;
     }
-    public static MembresApli lectureVille (File fichier) throws IOException{
-        MembresApli membresApli = new MembresApli();
 
+    public static MembresApli lectureVille (File fichier) throws IOException{
+
+        MembresApli membresApli = new MembresApli();
         BufferedReader bufferEntree = new BufferedReader(new FileReader(fichier));
         String ligne;
-
         StringTokenizer tokenizer;
+
         do {
             ligne = bufferEntree.readLine();
             if (ligne != null) {
                 tokenizer = new StringTokenizer(ligne, " ");
-                membresApli.ajoutMembresVilles(tokenizer.nextToken(), tokenizer.nextToken());
+                membresApli.ajoutMembresVilles(tokenizer.nextToken(), tokenizer.nextToken()); /** Ajout des deux tokens Membre et Ville
+                 à l'objet de la classe MembreApli **/
             }
         }
+
         while (ligne != null);
         bufferEntree.close();
         return membresApli;
@@ -44,11 +51,12 @@ public class LectureEcriture {
 
     public static void ecritureScenario (String nomFichier, Scenario scenario) throws IOException{
 
-        PrintWriter sortie = new PrintWriter(new BufferedWriter((new FileWriter(nomFichier))));
+        PrintWriter sortie = new PrintWriter(new BufferedWriter((new FileWriter(nomFichier)))); // Création de l'objet qui va permettre de modifier le fichier
         int i = 0;
-        for (String vendeur : scenario.getListVendeurs()) {
+        for (String vendeur : scenario.getListVendeurs()) { // Parcours la liste des vendeurs et ajoute l'acheteur correspondant
             sortie.println(vendeur + " ->" + scenario.getListAcheteurs().get(i));
         }
+
         sortie.close();
     }
 }
--- src/main/java/vue/CarteMembrePane.java
+++ src/main/java/vue/CarteMembrePane.java
@@ -15,45 +15,46 @@ import java.io.IOException;
 import java.util.List;
 
 public class CarteMembrePane extends GridPane {
-
-    private ComboBox membreSelection = new ComboBox<String>();
-    File filefeu = new File("C:/SAE_algo/badge_feu.png");
-    File fileau = new File("C:/SAE_algo/badge_eau.png");
-    File filemetal = new File("C:/SAE_algo/badge_metal.png");
-
-    String localUrl = filefeu.toURI().toURL().toString();
-    String localUrl2 = fileau.toURI().toURL().toString();
-    String localUrl3 = filemetal.toURI().toURL().toString();
-
-    Image badgefeu = new Image(localUrl);
-    Image badgeeau = new Image(localUrl2);
-    Image badgemetal = new Image(localUrl3);
     private MembresApli membresApli;
     private Label ville;
     private List<String> listVille;
     private Label num;
+    private ComboBox membreSelection = new ComboBox<String>();
 
     public CarteMembrePane() throws IOException {
-        File membreAp= new File("scenarios" + File.separator + "membres_APLI.txt");
-        MembresApli membresApli = LectureEcriture.lectureVille(membreAp);
+
+        File filefeu = new File("C:/SAE_algo/badge_feu.png"); // Fichier contenant le grade du membre de l'APLI
+        File fileau = new File("C:/SAE_algo/badge_eau.png");
+        File filemetal = new File("C:/SAE_algo/badge_metal.png");
+
+        String localUrl = filefeu.toURI().toURL().toString(); // URl de l'image contenant le grade du membre de l'APLI
+        String localUrl2 = fileau.toURI().toURL().toString();
+        String localUrl3 = filemetal.toURI().toURL().toString();
+
+        Image badgefeu = new Image(localUrl); // Image contenant le grade du membre de l'APLI
+        Image badgeeau = new Image(localUrl2);
+        Image badgemetal = new Image(localUrl3);
+
+        MembresApli membresApli = LectureEcriture.lectureVille(new File("membres" + File.separator + "membres_APLI.txt"));
+        // Création d'un objet MembresApli contenant une liste de membres et une liste de Ville
         listVille = MembresApli.getListVilles();
 
-        for (String member : MembresApli.getListMembres()){
+        for (String member : MembresApli.getListMembres()){ // Création de la liste déroulante contenant le nom des membres
             membreSelection.getItems().add(member);
         }
 
-        membreSelection.getSelectionModel().select(0);
-        this.add(membreSelection,0,0,4,1);
-        ville = new Label("Ville du membre : "+listVille.get(0));
+        membreSelection.getSelectionModel().select(0); // Mets par défaut la valeur de la liste déroulante au premier membre de lAPLI
+        ville = new Label("Ville du membre : "+listVille.get(0)); // Mets par défaut la ville du premier membre de l'APLI
+        num = new Label("numéro de membre : "+(membreSelection.getSelectionModel().getSelectedIndex()+1)); // Mets par défaut le numéro de membre à 1
+        ImageView imageView = new ImageView(badgeeau); // Mets par défaut le badge du membre à eau
+
+        this.add(membreSelection,0,0,4,1); // ajout de la liste déroulante à l'interface grahique ( a carteMembrePane)
         this.add(ville,0,1,4,1);
-
-        num = new Label("numéro de membre : "+(membreSelection.getSelectionModel().getSelectedIndex()+1));
         this.add(num,0,2,4,1);
-
-        ImageView imageView = new ImageView(badgeeau);
         this.add(imageView,0,3,1,1);
 
-        membreSelection.setOnAction(new EventHandler<ActionEvent>() {
+        membreSelection.setOnAction(new EventHandler<ActionEvent>() { /** Modification du Prénom, du numéro de membre et du badge
+            // en fonction du Membre selectionné **/
             @Override
             public void handle(ActionEvent actionEvent) {
                 ville.setText("Ville du membre : "+listVille.get(membreSelection.getSelectionModel().getSelectedIndex()));
